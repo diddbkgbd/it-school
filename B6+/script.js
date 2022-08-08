3aea17440bc10585eb8e573dcc577e75fd3fe5a6
Dianalocal <dianavasko708@gmail.com>
2022-08-08 18:03:57 +0300

    qqqq

--- B6+/script.js
+++ B6+/script.js
@@ -10,30 +10,52 @@ function check(str){
 }
   
 
-function buildWrapper(teg){
-    let item = document.createElement(teg)
-    function wrap(text, attribute){
-        text = check(text)
-        item.textContent=text;
-        for(let key in attribute){
-            let att = check(attribute[key])
-            item.setAttribute(key,att)
-        }
-        return item
+// function buildWrapper(teg){
+//     let item = document.createElement(teg)
+//     function wrap(text, attribute){
+//         text = check(text)
+//         item.textContent=text;
+//         for(let key in attribute){
+//             let att = check(attribute[key])
+//             item.setAttribute(key,att)
+//         }
+//         return item
         
-    }
-    return wrap;
+//     }
+//     return wrap;
+// };   
+
+
+function buildWrapper(teg){
+  let item = "";
+
+  function wrap(text, attribute){
+      text = check(text)
+      let atts="";
+      for(let key in attribute){
+          let at = check(attribute[key])
+          let att=` ${key}="${at}"`
+          atts =atts+att
+      }
+
+      item=`<${teg}${atts}>${text}</${teg}>`
+      return item
+      
+  }
+  return wrap;
 };   
 
+
+
+
+
   var wrapP=buildWrapper("P");   
-  console.log( wrapP("Однажды в студёную зимнюю пору") );
+ console.log( wrapP("Однажды в студёную зимнюю пору") );
   // в консоль выводится строка "<P>Однажды в студёную зимнюю пору</P>"
-  var wrapP=buildWrapper("P");
- console.log( wrapP("Однажды в студёную зимнюю пору",{lang:"ru"}) );
-  // в консоль выводится строка "<P lang='ru'>Однажды в студёную зимнюю пору</P>"
-  var wrapP=buildWrapper("P");
-  console.log( wrapP("Однажды в <студёную> зимнюю пору") );
-//   // в консоль выводится строка "<P>Однажды в &lt;студёную&gt; зимнюю пору</P>"
-  var wrapH1=buildWrapper("H1"); // строим функцию для оборачивания текста в тег H1
-  console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
-//   // в консоль выводится строка "<H1 align='center' title='M&amp;M&apos;s'>СТИХИ</H1>"
+console.log( wrapP("Однажды в студёную зимнюю пору",{lang:"ru"}) );
+ // в консоль выводится строка "<P lang='ru'>Однажды в студёную зимнюю пору</P>"
+ console.log( wrapP("Однажды в <студёную> зимнюю пору") );
+ // в консоль выводится строка "<P>Однажды в &lt;студёную&gt; зимнюю пору</P>"
+   var wrapH1=buildWrapper("H1"); // строим функцию для оборачивания текста в тег H1
+   console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
+  // в консоль выводится строка "<H1 align='center' title='M&amp;M&apos;s'>СТИХИ</H1>"
