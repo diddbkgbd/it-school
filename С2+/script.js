932e855c666dec9d8dfb0bdae4eb5d7acb7b67a5
Dianalocal <dianavasko708@gmail.com>
2022-08-11 12:57:43 +0300

    ttrtrtr

--- С2+/script.js
+++ С2+/script.js
@@ -1,27 +1,32 @@
 function deepCopy(obj) {
-    let clObj=[];
+    let clObj={};
+    let clObjAr=[];
 
-    if (obj == null || typeof obj !== 'object'|| Number.isNaN(obj)||typeof(obj)=="string") {
+    if (obj == null || (typeof obj !== 'object' && typeof obj !== 'array')) {
         return obj
     }
+
     if (obj instanceof Array){
         for (let m=0; m<obj.length;m++){
-            if(obj[m] instanceof Array){
-                clObj[m] = deepCopy(obj[m]);
+            if(obj[m] instanceof Object){
+                clObjAr[m] = deepCopy(obj[m]);
                 continue;
             }
-            clObj[m] = obj[m];
+            clObjAr[m] = obj[m];
         }
-        
+        return clObjAr;
     }
-    for(const i in obj) {
-        if (obj[i] instanceof Object) {
-            clObj[i] = deepCopy(obj[i]);
-            continue;
+    if (obj instanceof Object){
+        for(const i in obj) {
+            if (obj[i] instanceof Object) {
+                clObj[i] = deepCopy(obj[i]);
+                continue;
+            }
+          
+            clObj[i] = obj[i];
         }
-      
-        clObj[i] = obj[i];
+        return clObj;
     }
-    return clObj;
 }
+    
 
