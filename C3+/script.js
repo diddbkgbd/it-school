0142ca6f68ee84d15572c56ece789196ff30d676
diana <dianavasko708@gmail.com>
2022-08-19 00:12:07 +0300

    trtrttr

--- C3+/script.js
+++ C3+/script.js
@@ -4,17 +4,38 @@ function deepComp(obj, obj2){
         if ((obj instanceof Array && !(obj2 instanceof Array))|| (obj2 instanceof Array && !(obj instanceof Array))){
             return false
         }
+        if (obj instanceof Array && obj2 instanceof Array ){
+            if(obj.length!=obj2.length){
+                return false
+            }
+
+            for( let i=0;i<obj.length;i++ ){
+                if(obj[i] instanceof Array){
+                    let int = deepComp(obj[i],obj2[i])
+                    if (int == false){
+                        return false
+                    }
+                    else{
+                        continue
+                    }
+                }
+                let ck1 =obj2.indexOf(obj[i])
+                let ck2 =obj.indexOf(obj2[i]) 
+                if((ck1 == -1 || ck2 == -1) && !(Number.isNaN(obj[i]) && Number.isNaN(obj2[i]))){
+                    return false
+                }
+            } 
+            return true
+        }
         if (obj instanceof Object && obj2 instanceof Object ){
             let keys1=Object.keys(obj)
-            keys1 = keys1.sort()
             let keys2=Object.keys(obj2)
-            keys2 = keys2.sort()
-
             if(keys1.length!=keys2.length){
                 return false
             }
 
             for( let i=0;i<keys1.length;i++ ){
+
                 if(obj[keys1[i]] instanceof Object){
                     let int = deepComp(obj[keys1[i]],obj2[keys1[i]])
                     if (int == false){
@@ -24,7 +45,7 @@ function deepComp(obj, obj2){
                         continue
                     }
                 }
-                if(keys1[i]!==keys2[i] || (obj[keys1[i]] !==obj2[keys1[i]] && !(Number.isNaN(obj[keys1[i]]) && Number.isNaN(obj2[keys1[i]])))){
+                if(!(keys1[i] in obj) ||!(keys1[i] in obj2) || (obj[keys1[i]] !==obj2[keys1[i]] && !(Number.isNaN(obj[keys1[i]]) && Number.isNaN(obj2[keys1[i]])))){
                     return false
                 }
             } 
