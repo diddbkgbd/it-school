2141205210d1da43640f9d1a9a92835ae2495cbe
Diana <dianavasko708.gmail.com>
2022-10-12 00:42:03 +0300

    12345678

---
+++ MY_project/log.js
@@ -0,0 +1,94 @@
+"use strict";
+
+  // SPA
+  window.onhashchange=switchToStateFromURLHash;
+  window.onbeforeunload=switchToStateFromURLHash;
+  
+  let SPAState={};
+
+function switchToStateFromURLHash() {
+    var URLHash=window.location.hash;
+
+    var stateStr=URLHash.substring(1);
+    
+    if ( stateStr!="" ) { 
+      let parts=stateStr.split("_")
+      SPAState={ pagename: parts[0] }; 
+      if(SPAState.pagename =='Game'&& gameStart ==true){
+        warning()
+        alert("Не сохраниться!!!!")
+      }
+      
+      console.log(SPAState.pagename+"_"+gameStart)
+    }
+    else{
+      if(gameStart ==true){
+        alert("Не сохраниться!!!!")
+      }
+      SPAState={pagename:'Main'};
+    }
+       // иначе показываем главную страницу
+
+    console.log('Новое состояние приложения:');
+    console.log(SPAState);
+
+    // обновляем вариабельную часть страницы под текущее состояние
+
+    let pageHTML_left="";
+    let pageHTML_right = "";
+    let IPAGE = document.getElementById("IPage")
+    let IpageRight = document.getElementById("IpageRight")
+    switch ( SPAState.pagename ) {
+      case 'Main':
+        pageHTML_left ='<img class="main_logo" src="images/main.svg" alt="logo">'
+        IPAGE.innerHTML=pageHTML_left;
+        pageHTML_right = '<form action=""> <fieldset> <legend>Info</legend> <label for="name">Введите свой Ник:</label> <input type="text" id= "Name"> </fieldset> <div class="points"> <input type="checkbox" name="agree" id="agree">     <label for="agree">Согласен обрабатывать данные </label></div><input id = "Game_page" type="button" onclick="switchToGamePage()"  value="Начать игру"></form>'
+        IpageRight.innerHTML=pageHTML_right;
+        break;
+
+      case 'Game':
+        pageHTML_left = ' <canvas id="myCanvas" width="1200" height="800"></canvas> '
+        IPAGE.innerHTML=pageHTML_left;
+         pageHTML_right+='<h1>Привет, Дорогой Друг!</h1> <p class="rules">Правила игры:</p> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ullam saepe libero tempora consectetur doloribus quo quia dolorum, quasi deserunt blanditiis asperiores possimus hic, id perspiciatis officia natus quae explicabo?</p> <input type="button" id="Start" value="Start!"> <input type="button" id="Pause" value="Pause!"> <input type="button" id="Results" value="Results!">' 
+        IpageRight.innerHTML=pageHTML_right;
+        START()
+        break;
+
+      case 'Record':
+        pageHTML_left = ' ResultTablr '
+        IPAGE.innerHTML=pageHTML_left;
+        pageHTML_right+='<h1>Поздравляю, Дорогой Друг!</h1> <p class="rules">Данные таблицы:</p> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ullam saepe libero tempora consectetur doloribus quo quia dolorum, quasi deserunt blanditiis asperiores possimus hic, id perspiciatis officia natus quae explicabo?</p> <input type="button" id="Main" value="Main!" onclick="switchToMainPage()"> ' 
+        IpageRight.innerHTML=pageHTML_right;
+        break;
+    }
+    // IPAGE.innerHTML=pageHTML;
+
+  }
+
+
+  function switchToState(newState) {
+
+    var stateStr=newState.pagename;
+  
+    location.hash=stateStr;
+  }
+
+  function switchToMainPage() {
+    switchToState( { pagename:'Main' } );
+  }
+
+  function switchToGamePage() {
+    GetName()
+    switchToState( { pagename:'Game' } );
+  }
+
+  function switchToResultsPage() {
+    switchToState( { pagename:'Record' } );
+  }
+
+  switchToStateFromURLHash();
+
+ function GetName(){
+    const NICK = document.getElementById("Name").value;
+    console.log (NICK)
+ }
---
+++ MY_project/script.js
@@ -0,0 +1,289 @@
+// "use stict"
+
+//САМА ИГРА!!!!!!
+
+let gameStart = false
+function warning(EO){
+    EO = EO||window.event
+    if (gameStart ){
+        EO.returnValue='А у вас есть несохранённые изменения!';
+    };
+
+}
+function START (){
+    gameStart=false
+
+    window.onbeforeunload= warning
+    window.onhashchange=switchToStateFromURLHash
+
+   
+    console.error("START!!!!")
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    let gameRun = false
+   
+
+    let startButton = document.getElementById("Start")
+    let pauseButton = document.getElementById("Pause")
+    let resultsButton = document.getElementById("Results")
+
+
+     
+  
+
+    let Ball = {
+        x : (canvas.width-80)/2,
+        y: canvas.height-80-40,
+        width: 80, 
+        height:80, 
+        radius: 80, 
+        dx: 4, 
+        dy:4
+    }
+    
+    let Paddle = {
+        width: 150, 
+        height: 20, 
+        x: (canvas.width-150)/2,
+        y: canvas.height - 20
+    }
+    
+    let Brick ={
+        row:5,
+        column: 3, 
+        width: 200, 
+        height: 50, 
+        padding: 40, 
+        offsetTop: 60, 
+        offsetLeft: 20
+    }
+    
+    var rightPressed = false;
+    var leftPressed = false;
+    
+
+    let ball=new Image();
+    ball.src='images/ball.png';
+
+    var brick_img=new Image();
+    brick_img.src='images/main_5.jpg';
+    
+    var score = 0;
+    var lives = 3;
+ 
+    var bricks = [];
+    for(var c=0; c<Brick.column; c++) {
+      bricks[c] = [];
+      for(var r=0; r<Brick.row; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+      }
+    }
+    
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+    canvas.addEventListener("mousemove", mouseMoveHandler, false);
+
+   
+    
+    
+    function keyDownHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = true;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = true;
+        }
+    }
+    
+    function keyUpHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = false;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = false;
+        }
+    }
+    
+    function mouseMoveHandler(e) {
+    
+      var relativeX = e.clientX - canvas.offsetLeft;
+      if(relativeX > 0 && relativeX < canvas.width) {
+        Paddle.x = relativeX - Paddle.width/2;
+      }
+    }
+    
+    
+    function collisionDetection() {
+      for(var c=0; c<Brick.column; c++) {
+        for(var r=0; r<Brick.row; r++) {
+          var b = bricks[c][r];
+          if(b.status == 1) {
+            if(Ball.x +Ball.width> b.x && Ball.x < b.x+Brick.width && Ball.y+Ball.height > b.y && Ball.y < b.y+Brick.height) {
+              Ball.dy = -Ball.dy;
+              b.status = 0;
+              score++;
+              if(score == Brick.column*Brick.row) {
+                alert("YOU WIN, CONGRATS!");
+                resultsGame()
+                
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    
+  
+ 
+
+    function drawBall() {
+        ctx.drawImage(ball,Ball.x,Ball.y,Ball.radius,Ball.radius);
+    }
+    
+    function drawPaddle() {
+        
+      ctx.beginPath();
+      ctx.rect(Paddle.x, canvas.height-Paddle.height, Paddle.width, Paddle.height);
+      ctx.fillStyle = "#DAA520";
+      ctx.fill();
+      ctx.closePath();
+    }
+    
+    
+    
+    
+    function drawBricks() {
+      for(var c=0; c<Brick.column; c++) {
+        for(var r=0; r<Brick.row; r++) {
+          if(bricks[c][r].status == 1) {
+            var brickX = (r*(Brick.width+Brick.padding))+Brick.offsetLeft;
+            var brickY = (c*(Brick.height+Brick.padding))+Brick.offsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+        
+            ctx.drawImage(brick_img, brickX, brickY, Brick.width, Brick.height);
+          }
+        }
+      }
+    }
+    function drawScore() {
+      ctx.font = "16px Arial";
+      ctx.fillStyle = "#0095DD";
+      ctx.fillText("Score: "+score, 8, 20);
+    }
+    function drawLives() {
+      ctx.font = "16px Arial";
+      ctx.fillStyle = "#0095DD";
+      ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    }
+    
+    
+    
+    function collision(objA, objB) {
+    
+        if(objA.x <= objB.x + objB.width && objA.x + objA.width >= objB.x && objA.y <= objB.y + objB.height &&
+            objA.height + objA.y >= objB.y){
+    
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+    
+    function draw() {
+        window.onhashchange=switchToStateFromURLHash
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawPaddle();
+      drawBricks();
+      drawBall();
+      drawScore();
+      drawLives();
+      collisionDetection()
+    //   collision()
+        if (collision(Ball,Paddle)==true){
+            Ball.dy = -Ball.dy
+        }
+    // отбивание от крайов
+      if(Ball.x + Ball.dx > canvas.width-Ball.radius || Ball.x + Ball.dx < 0) {
+        Ball.dx = -Ball.dx;
+      }
+      if(Ball.y + Ball.dy < 0) {
+        Ball.dy = -Ball.dy;
+      }
+      else if(Ball.y + Ball.dy > canvas.height-Ball.radius) {
+        if(Ball.x > Paddle.x && Ball.x < Paddle.x + Paddle.width) {//!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Ball.dy = -Ball.dy;
+        }
+        else {
+          lives--;
+          if(!lives) {
+            alert("GAME OVER");
+            document.location.reload();
+          }
+          else {
+            Ball.x = (canvas.width-80)/2;
+            Ball.y = canvas.height-80-30;
+            Ball.dx = 3;
+            Ball.dy = -3;
+            Paddle.x = (canvas.width-Paddle.width)/2;
+          }
+        }
+      }
+    
+      if(rightPressed && Paddle.x < canvas.width-Paddle.width) {
+        Paddle.x += 7;
+      }
+      else if(leftPressed && Paddle.x > 0) {
+        Paddle.x -= 7;
+      }
+    
+      Ball.x += Ball.dx;
+      Ball.y += Ball.dy;
+
+
+      if (gameRun){
+        requestAnimationFrame(draw);
+      }
+   
+    }
+   
+    
+    
+    ball.onload = function(){
+        startButton.addEventListener("click", startGame) 
+        brick_img.onload = draw      
+    }
+    function startGame(){
+        pauseButton.addEventListener("click", pauseGame)
+        startButton.style.display = "none"
+        pauseButton.style.display = "block"
+        gameRun = true;
+        gameStart=true;
+        console.log (gameStart)
+        draw()
+    }
+
+    function pauseGame(){
+        gameRun = false
+        startButton.style.display = "block"
+        pauseButton.style.display = "none"
+    }
+    function resultsGame(){
+        pauseButton.style.display = "none"
+        resultsButton.style.display = "block"
+        gameRun=false;
+        resultsButton.addEventListener("click", switchToResultsPage)
+        // draw()
+        // document.location.reload();
+    }
+ }
+
+
+
+
